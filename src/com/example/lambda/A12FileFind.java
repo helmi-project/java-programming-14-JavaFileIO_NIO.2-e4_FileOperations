09b173499342dc13816030215a1aa30c2ca65a04
Helmi Hartanto <>
2021-01-08 15:40:04 +0700

    First Commit

---
+++ src/com/example/lambda/A12FileFind.java
@@ -0,0 +1,24 @@
+package com.example.lambda;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class A12FileFind {
+    
+  public static void main(String[] args) {
+                
+    try(Stream<Path> files = 
+      Files.find(
+          Paths.get("."), 9, (p, a) -> a.isDirectory())){          
+            
+      files
+          .forEach(f -> System.out.println("Dirname: " + f));
+            
+    } catch (IOException e){
+      System.out.println("Message: " + e.getMessage());
+    }        
+  }
+}
